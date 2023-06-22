e15de51450b3aae0ba0cea99e773143b195c5f22
Thang Truong <thang.truong.swin@gmail.com>
2023-06-22 18:16:06 +1000

    Done - design search input component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import reactlogo from "../assets/react.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
 	return (
-		<HStack justifyContent="space-between" px="15px" my="15px">
+		// justifyContent="space-between"
+		<HStack px="15px" my="15px">
 			<Image src={reactlogo} boxSize="40px"></Image>
+			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
 	);
